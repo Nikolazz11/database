d5f017c9b044f688d82ca1083855ade4882a264c
Benjamin9854 <110208863+Benjamin9854@users.noreply.github.com>
2024-05-06 13:57:52 -0300

    Update index.php

--- index.php
+++ index.php
@@ -14,20 +14,33 @@
 <body>
     <p class="text-3xl">Relacionado con ventas</p>
     <p class="text-xl">1. ¿Que ventas se realizaron del 2020 en adelante?</p>
-    <?php while($fila = mysqli_fetch_array($respuesta1))
-    {
+    <button onclick="toggleAnswer()">Mostrar/Ocultar Respuesta</button>
+    <div id="respuesta" style="display: none;">
+        <?php while($fila = mysqli_fetch_array($respuesta1))
+        {
+            ?>
+            <tr>
+                <td"><?php echo $fila['ID_ventas']?></td>
+                <td><?php echo $fila['Fecha']?></td>
+                <td><?php echo $fila['ID_Cliente']?></td>
+                <td><?php echo $fila['ID_Producto']?></td>
+                <td><?php echo $fila['Cantidad']?></td>
+                <td><?php echo $fila['Precio_Unitario']?></td>
+            </tr>
+            <br>
+            <?php
+        }
         ?>
-        <tr>
-            <td"><?php echo $fila['ID_ventas']?></td>
-            <td><?php echo $fila['Fecha']?></td>
-            <td><?php echo $fila['ID_Cliente']?></td>
-            <td><?php echo $fila['ID_Producto']?></td>
-            <td><?php echo $fila['Cantidad']?></td>
-            <td><?php echo $fila['Precio_Unitario']?></td>
-        </tr>
-        <br>
-        <?php
-    }
-    ?>
+    </div>
+    <script>
+        function toggleAnswer() {
+            var respuesta = document.getElementById("respuesta");
+            if (respuesta.style.display === "none") {
+                respuesta.style.display = "block";
+            } else {
+                respuesta.style.display = "none";
+            }
+        }
+    </script>
 </body>
 </html>
